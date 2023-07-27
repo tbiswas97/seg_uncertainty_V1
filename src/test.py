5b93445a5f973b68bc2dea460d52dda18033fe15
Tridib Biswas <tb@Tridibs-MBP.fios-router.home>
2023-07-27 11:13:20 -0400

    running fit_model with 4 components for all images

--- src/test.py
+++ src/test.py
@@ -1,16 +1,17 @@
+import numpy as np
 import import_utils
 from SegmentationMap import SegmentationMap as SM 
 
-#for idx,iid in enumerate(import_utils.IIDS):
-    #a = SM(iid)
-    #print("Fitting image id: {} | idx {} out of {}".format(iid,idx,len(import_utils.IIDS)))
-    #a.fit_model()
+for idx,iid in enumerate(import_utils.IIDS):
+    a = SM(iid)
+    print("Fitting image id: {} | idx {} out of {}".format(iid,idx,len(import_utils.IIDS)))
+    a.fit_model(n_components=np.array([4]))
 
-    #import_utils._pickle(a,'SegmentationMap_{}_{}.pkl'.format(iid,idx))
+    import_utils._pickle(a,'SegmentationMap_{}_{}.pkl'.format(iid,idx))
 
-idxs = [6]
-for idx in idxs:
-    a = SM(import_utils.IIDS[idx])
-    print("fitting...")
-    a.fit_model()
-    import_utils._pickle(a,'SegmentationMap_{}_{}.pkl'.format(import_utils.IIDS[idx],idx))
+#idxs = [2]
+#for idx in idxs:
+    #a = SM(import_utils.IIDS[idx])
+    #print("fitting...")
+    #a.fit_model(n_components=np.array([3]))
+    #import_utils._pickle(a,'SegmentationMap_{}_{}.pkl'.format(import_utils.IIDS[idx],idx))
