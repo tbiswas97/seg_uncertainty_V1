0b41961af5ad661e4de8cea286007c3fdad24c12
Tridib Biswas <tb@Tridibs-MBP.fios-router.home>
2023-08-10 12:54:59 -0400

    ready to fit each image with best k

--- src/fit_model_best_k.py
+++ src/fit_model_best_k.py
@@ -4,8 +4,10 @@ from SegmentationMap import SegmentationMap as SM
 
 for idx,iid in enumerate(import_utils.IIDS):
     a = SM(iid)
-    print("Fitting image id: {} | idx {} out of {}".format(iid,idx,len(import_utils.IIDS)))
-    a.fit_model(model="c",n_components=np.array([3,4,5]))
+    k_arr = np.asarray(a.k)
+    best_k = round(np.median(k_arr[k_arr<10]))
+    print("Fitting image id: {} for {} components| idx {} out of {}".format(iid,best_k,idx,len(import_utils.IIDS)))
+    a.fit_model(model="c",n_components=np.array([best_k]))
 
     import_utils._pickle(a,'SegmentationMap_k345_{}_{}.pkl'.format(iid,idx))
 
